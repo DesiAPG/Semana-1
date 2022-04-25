20e3215db55c89fe581e20f6acfa647ddd443593
DesiAPG <desireapgames@gmail.com>
2022-04-25 12:41:18 -0500

    'HTTP Methods'

--- index.js
+++ index.js
@@ -2,5 +2,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("Hello world");
+  res.send("Hello world");
+});
+
+app.post("/usuarios", (req, res) => {
+  res.send("Metodo Post");
+});
+
+app.put("/usuarios", (req, res) => {
+  res.send("Metodo Put");
+});
+
+app.delete("/usuarios", (req, res) => {
+  res.send("Metodo Delete");
+});
+
+app.listen(4000, () => {
+  console.log("Escuchando en el puerto 4000");
 });
