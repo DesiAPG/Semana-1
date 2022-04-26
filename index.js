d558456ddd20ea267de670173944d3c53107338c
DesiAPG <desireapgames@gmail.com>
2022-04-26 08:04:09 -0500

    'Installed nodemon and middlewares'

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 
+app.use(express.text());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.post("/usuarios", (req, res) => {
-  res.send("Metodo Post");
+  const user = req.body;
+  console.log(user);
+  res.send("Metodo Post" + user);
 });
 
-app.put("/usuarios", (req, res) => {
+app.post("/json", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  res.send("Metodo Post" + user);
+});
+
+app.put("/", (req, res) => {
   res.send("Metodo Put");
 });
 
-app.delete("/usuarios", (req, res) => {
+app.delete("/", (req, res) => {
   res.send("Metodo Delete");
 });
 
